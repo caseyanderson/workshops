f2636ca285d3ae186b968ef19197c41415965a84
caseyanderson <casey.thomas.anderson@gmail.com>
2015-02-05 13:47:01 -0800

    barely out of sync, just doing this as a hack

--- intrmdt_python/intrmd_python_samples.py
+++ intrmdt_python/intrmd_python_samples.py
@@ -1,3 +1,7 @@
 def helloWorld():
     """Greets the world upon creation"""
     print("HELLO WORLD!")
+
+def mult( a, b ):
+    """Multiplies the first parameter by the second parameter, returns the result"""
+    return a * b
