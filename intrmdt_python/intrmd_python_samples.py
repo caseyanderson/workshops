4b90921b5827869a93ea503a4975480bd532b997
caseyanderson <casey.thomas.anderson@gmail.com>
2015-02-05 12:56:38 -0800

    wrapping up functions section, on to objects and classes

--- intrmdt_python/intrmd_python_samples.py
+++ intrmdt_python/intrmd_python_samples.py
@@ -1,3 +1,23 @@
 def helloWorld():
     """Greets the world upon creation"""
     print("HELLO WORLD!")
+
+
+'''Function with parameter.'''
+
+def happyBirthday(person):
+    """formats a happy birthday message with the birthday person's name"""
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear " + person + ".")
+    print("Happy Birthday to you!")
+
+def happyBirthday(person):
+        """formats a happy birthday message with the birthday person's name"""
+        for i in range(4):
+            if i != 2:
+                message = "Happy Birthday to you!"
+                return message
+            else:
+                message = "Happy Birthday, dear " + person + "!"
+                return message
